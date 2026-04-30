915f1d7f7fec093bac48f25d85397d5df35aca76
JAY-cloudbuster <kjayeshrao@gmail.com>
2026-04-30 23:17:08 +0530

    the methods and the functions with scanner inputs clarification

--- src/methods_functions_java/sample.java
+++ src/methods_functions_java/sample.java
@@ -1,4 +1,16 @@
 package methods_functions_java;
+import java.util.*;
 
 public class sample {
+    public static void main(String[] args){
+        sum();
+    }
+
+    static void sum(){ //here since the numbers are taking input and being used inside only no nnedd for the declaration parameters? or explain why have not used the parameters
+        Scanner input = new Scanner(System.in); //is this called method?
+        System.out.println("enter the values: ");
+        // to take the array asinput cannot automate the array values in the system
+        // need to write the code using the for loop logic and conventional input code
+    }
+
 }
