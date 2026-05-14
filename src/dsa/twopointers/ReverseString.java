39ed29ea5c364be2081d87dce7c9a337daa13e8d
KALLA JAYESH RAO <cb.sc.u4cse23418@cb.students.amrita.edu>
2026-05-14 23:18:44 +0530

    the leetcode problem Reverse the String

---
+++ src/dsa/twopointers/ReverseString.java
@@ -0,0 +1,11 @@
+package dsa.twopointers;
+import java.util.*;
+public class ReverseString {
+    public static void main(String[] args){
+        char[] list = {'q', 'r', 's'};
+
+        System.out.println(list.length);
+        System.out.println(list[1]);
+
+    }
+}
